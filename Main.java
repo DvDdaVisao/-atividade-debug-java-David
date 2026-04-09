0b50fa0ee99658c7dc76823d15950fd1f58d04b5
DvDdaVisao <dfgamerwolf12@gmail.com>
2026-04-09 09:26:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("------====[ C A L C U L A D O R A]====------\n\n");
+
+        double total= 10;
+
+        System.out.println("Digite um número:");
+        double num1 = ler.nextDouble();
+
+        System.out.println("Digite outro número:");
+        double num2 = ler.nextDouble();
+
+        System.out.println("Digite a operaç\não:\n(+, -, / e x)");
+        String opera = ler.next();
+
+        switch (opera) {
+            case "+":
+                total=num1+num2;
+                System.out.println("Resultado = "+total);
+                break;
+            case "-":
+                total=num1/num2;
+                System.out.println("Resultado = "+total);
+            case "/":
+                total=num1-num2;
+                System.out.println("Resultado = "+total);
+                break;
+            case "*":
+                total=num1*num2;
+                System.out.println("Resultado = "+num1);
+                break;
+        }
+        }
+    }
